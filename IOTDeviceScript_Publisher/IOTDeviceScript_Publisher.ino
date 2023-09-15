a4ba605f01bb5f8f0e38ca89b1f8e2eccd7577f5
CharlyMoraUA <ca.morar1@uniandes.edu.co>
2023-09-14 20:41:21 -0500

    cambio de usuario

--- IOTDeviceScript_Publisher/IOTDeviceScript_Publisher.ino
+++ IOTDeviceScript_Publisher/IOTDeviceScript_Publisher.ino
@@ -45,12 +45,12 @@ const char ssid[] = "PECHAN"; // TODO cambiar por el nombre de la red WiFi
 const char pass[] = "Carlos123"; // TODO cambiar por la contraseña de la red WiFi
 
 //Conexión a Mosquitto
-#define USER "user1" // TODO Reemplace UsuarioMQTT por un usuario (no administrador) que haya creado en la configuración del bróker de MQTT.
+#define USER "pe.perez" // TODO Reemplace UsuarioMQTT por un usuario (no administrador) que haya creado en la configuración del bróker de MQTT.
 const char MQTT_HOST[] = "34.204.74.226"; // TODO Reemplace ip.maquina.mqtt por la IP del bróker MQTT que usted desplegó. Ej: 192.168.0.1
 const int MQTT_PORT = 8082;
 const char MQTT_USER[] = USER;
 //Contraseña de MQTT
-const char MQTT_PASS[] = "123456"; // TODO Reemplace ContrasenaMQTT por la contraseña correpondiente al usuario especificado.
+const char MQTT_PASS[] = "abc123"; // TODO Reemplace ContrasenaMQTT por la contraseña correpondiente al usuario especificado.
 
 //Tópico al que se recibirán los datos
 // El tópico de publicación debe tener estructura: <país>/<estado>/<ciudad>/<usuario>/out
